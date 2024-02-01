97b0072e64cb6cf51638b34c337837f338386837
waqas <wmunir232@gmail.com>
2024-02-01 16:36:29 +0500

    Editied Homepage with tailwind

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,23 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./Home.module.css";
+import { Button } from "../@/components/button";
 
 export default function Home() {
   const navigate = useNavigate();
   return (
-    <main className={styles.home}>
-      <h2>
+    <main className="flex flex-col h-[85vh] items-center space-y-6 justify-center">
+      <h2 className="text-3xl w-[70%] text-center">
         Empower Your Productivity: Your Tasks, Your Way - Effortless
-        Organization with SyncUP
+        Organization with <span className="italic">SyncUP</span>
       </h2>
-      <button
+      <Button
+        variant="secondary"
         onClick={() => {
           navigate("/auth/login");
         }}
       >
         Login
-      </button>
+      </Button>
     </main>
   );
 }
